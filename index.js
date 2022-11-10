0ac41435fe802c33e689a8ab01e5625c66deb593
KIT WEI <ongkitwei@gmail.com>
2022-11-10 23:25:30 +0800

    rm bullet pt and style list

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 
 
 //if u dw to write onclick in html can write like that
-let myLeads = ["www.awesomelead.com ", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el") ;
 const inputBtn = document.getElementById("input-btn") ;
 const ulEl = document.getElementById("ul-el") ;
+
 
 inputBtn.addEventListener("click", function(){             //when the save button is clicked do this
     console.log("button clicked") ;
     myLeads.push(inputEl.value);
     console.log(myLeads);
+    console.log(myLeads.length);
+    inputEl.value = " " ;
+    showlist();
 })
 
 
-
-for( let i =0; i<myLeads.length; i++)
+function showlist(){
+    
+    let ulList = " "; 
+    for( let i =0; i<myLeads.length; i++)
 {
     //console.log(myLeads[i])
-    //ulEl.innerHTML = "<li>"  + myLeads[i] +  ulEl.innerHTML + "</li>" ;     //use innerHtml to insert html tags in js code
-   
-   
-
-
+    ulList += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>` ;
+    ulEl.innerHTML = ulList;//use innerHtml to insert html tags in js code
     //alternative way
 
-    const li = document.createElement("li")
+    /*const li = document.createElement("li")
     li.textContent = myLeads[i] ;
-    ulEl.append(li) ;
-    console.log
+    ulEl.append(li) ;*/
+    
 }
+}
+
